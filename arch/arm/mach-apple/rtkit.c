9b6b6ed5a086a83f0e372ee62a88b892e9c2c830
Mark Kettenis <kettenis@openbsd.org>
2021-12-18 18:50:12 +0100

    apple: fix rtkit code

--- arch/arm/mach-apple/rtkit.c
+++ arch/arm/mach-apple/rtkit.c
@@ -12,6 +12,10 @@
 #include <linux/bitfield.h>
 
 #define APPLE_RTKIT_EP_MGMT 0
+#define APPLE_RTKIT_EP_CRASHLOG	1
+#define APPLE_RTKIT_EP_SYSLOG 2
+#define APPLE_RTKIT_EP_DEBUG 3
+#define APPLE_RTKIT_EP_IOREPORT 4
 
 #define APPLE_RTKIT_MGMT_TYPE GENMASK(59, 52)
 
@@ -37,6 +41,9 @@
 #define APPLE_RTKIT_MIN_SUPPORTED_VERSION 11
 #define APPLE_RTKIT_MAX_SUPPORTED_VERSION 12
 
+/* Messages for internal endpoints. */
+#define APPLE_RTKIT_BUFFER_REQUEST 1
+
 phys_addr_t apple_rtkit_phys_start;
 phys_addr_t apple_rtkit_phys_addr;
 phys_size_t apple_rtkit_size;
@@ -56,9 +63,9 @@ int apple_rtkit_init(struct mbox_chan *chan, int wakeup,
 	int i, ret;
 
 	if (apple_rtkit_phys_start == 0) {
-		apple_rtkit_phys_start = (phys_addr_t)memalign(SZ_512K, SZ_64K);
+		apple_rtkit_phys_start = (phys_addr_t)memalign(SZ_16K, SZ_256K);
 		apple_rtkit_phys_addr = apple_rtkit_phys_start;
-		apple_rtkit_size = SZ_512K;
+		apple_rtkit_size = SZ_256K;
 	}
 
 	/* EP0_IDLE */
@@ -141,6 +148,10 @@ wait_epmap:
 		goto wait_epmap;
 
 	for (i = 0; i < nendpoints; i++) {
+		/* Don't start the syslog endpoint since we can't handle
+		   easily handle its messages in U-Boot. */
+		if (endpoints[i] == APPLE_RTKIT_EP_SYSLOG)
+			continue;
 		/* EP0_SEND_EPSTART */
 		msg.msg0 = FIELD_PREP(APPLE_RTKIT_MGMT_TYPE, APPLE_RTKIT_MGMT_STARTEP) |
 			FIELD_PREP(APPLE_RTKIT_MGMT_STARTEP_EP, endpoints[i]) |
@@ -160,28 +171,42 @@ wait_epmap:
 		msgtype = FIELD_GET(APPLE_RTKIT_MGMT_TYPE, msg.msg0);
 
 		if (endpoint == APPLE_RTKIT_EP_MGMT && msgtype == 11)
-			continue;
+			break;
 
-		if (endpoint == 1 || endpoint == 2 || endpoint == 4) {
+		if (endpoint == APPLE_RTKIT_EP_CRASHLOG ||
+		    endpoint == APPLE_RTKIT_EP_SYSLOG ||
+		    endpoint == APPLE_RTKIT_EP_IOREPORT) {
 			u64 size = ((msg.msg0 >> 44) & 0xff) << 12;
+			u64 addr = (msg.msg0 & 0xfffffffffff);
 
-			if (apple_rtkit_phys_addr + size >
-			    apple_rtkit_phys_start + apple_rtkit_size) {
-				printf("%s: out of memory\n", __func__);
-				return -ENOMEM;
-			}
+			if (msgtype == APPLE_RTKIT_BUFFER_REQUEST && addr != 0)
+				continue;
 
-			if (sart_map) {
-				ret = sart_map(cookie, apple_rtkit_phys_addr,
-					       size);
-				if (ret < 0)
-					return ret;
+			if (endpoint != APPLE_RTKIT_EP_IOREPORT &&
+			    msgtype != APPLE_RTKIT_BUFFER_REQUEST)
+				continue;
+
+			size = roundup(size, SZ_16K);
+			if (addr == 0 && size > 0 && msgtype == 1) {
+				if (apple_rtkit_phys_addr + size >
+				    apple_rtkit_phys_start + apple_rtkit_size) {
+					printf("%s: out of memory\n", __func__);
+					return -ENOMEM;
+				}
+
+				addr = apple_rtkit_phys_addr;
+				apple_rtkit_phys_addr += size;
+
+				if (sart_map) {
+					ret = sart_map(cookie, addr, size);
+					if (ret < 0)
+						return ret;
+				}
 			}
 
 			msg.msg0 &= ~0xfffffffffff;
-			msg.msg0 |= apple_rtkit_phys_addr;
+			msg.msg0 |= addr;
 			mbox_send(chan, &msg);
-			apple_rtkit_phys_addr += size;
 			continue;
 		}
 		if (endpoint != APPLE_RTKIT_EP_MGMT) {
